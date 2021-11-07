ec4ac972dc8dc2b735e1360e8bdf94d4f9977c46
Adam Siegel <tobioatom@gmail.com>
2021-11-07 13:00:49 -0800

    added solve

--- HTB/misc/misDIRection/decoder.js
+++ HTB/misc/misDIRection/decoder.js
@@ -6,18 +6,19 @@ secret[16] =  5;
 secret[36] =  9;
 secret[23] = 'B';
 secret[4] =  'C';
-secret[13] = 'd'; secret[26] = 'd';
+secret[13] = 'd'; secret[26] = 'D';
 secret[5] =  'e'; secret[14] = 'E';
 secret[2] =  'F'; secret[19] = 'F'; secret[27] = 'F';
 secret[8] =  'J'; secret[10] = 'j'; secret[12] = 'j';
 secret[11] = 'N'; secret[25] = 'N'; secret[31] = 'N'; secret[33] = 'N';
 secret[32] = 'p';
 secret[3] = 'R'; secret[7] = 'R';
-secret[1] = 'S'; secret[24] = 'S';
-secret[9] = 'u';secret[20] = 'u'; secret[28] = 'u';
+secret[1] = 'S'; secret[24] = 's';
+secret[9] = 'U';secret[20] = 'u'; secret[28] = 'u';
 secret[35] = 'V';
 secret[15] = 'x'; secret[17] = 'X'; secret[21] = 'X'; secret[29] = 'X';
 secret[18] = 'z';
 
-secret.shift();
+// dirnames were mangled in my zip so i used the string found here to solve https://cyruslab.net/2020/04/22/hacktheboxmisdirection/
+atob(secret.join(''));
 
