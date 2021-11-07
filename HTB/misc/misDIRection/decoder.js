8d504d0002ab7f10248fc542641c39467351de97
Adam Siegel <tobioatom@gmail.com>
2021-11-07 12:55:54 -0800

    added solve

---
+++ HTB/misc/misDIRection/decoder.js
@@ -0,0 +1,23 @@
+const secret = [];
+secret[6]  =  0;
+secret[22] =  1;  secret[30] =  1;
+secret[34] =  2;
+secret[16] =  5;
+secret[36] =  9;
+secret[23] = 'B';
+secret[4] =  'C';
+secret[13] = 'd'; secret[26] = 'd';
+secret[5] =  'e'; secret[14] = 'E';
+secret[2] =  'F'; secret[19] = 'F'; secret[27] = 'F';
+secret[8] =  'J'; secret[10] = 'j'; secret[12] = 'j';
+secret[11] = 'N'; secret[25] = 'N'; secret[31] = 'N'; secret[33] = 'N';
+secret[32] = 'p';
+secret[3] = 'R'; secret[7] = 'R';
+secret[1] = 'S'; secret[24] = 'S';
+secret[9] = 'u';secret[20] = 'u'; secret[28] = 'u';
+secret[35] = 'V';
+secret[15] = 'x'; secret[17] = 'X'; secret[21] = 'X'; secret[29] = 'X';
+secret[18] = 'z';
+
+secret.shift();
+
